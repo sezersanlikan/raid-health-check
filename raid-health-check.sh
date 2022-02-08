d40dbc116aca2edd33841cbb7e39787ebd540d44
Sezer Şanlıkan <87486491+sezersanlikan@users.noreply.github.com>
2022-02-08 21:02:02 +0300

    Create raid-health-check.sh

---
+++ raid-health-check.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+if [[ `/opt/MegaRAID/MegaCli/MegaCli64 -AdpAllInfo -aAll | grep "Critical Disk" | awk {'print $4'}` -eq 0 ]];
+then
+    echo "You have `/opt/MegaRAID/MegaCli/MegaCli64 -AdpAllInfo -aAll | grep "Critical Disk" | awk {'print $4'}` disk in Critical Condition" >> /var/log/raid-health-check.log ;
+fi
+if [[ `/opt/MegaRAID/MegaCli/MegaCli64 -AdpAllInfo -aAll | grep "Failed Disk" | awk {'print $4'}` -eq 0 ]];
+then
+    echo "You have `/opt/MegaRAID/MegaCli/MegaCli64 -AdpAllInfo -aAll | grep "Critical Disk" | awk {'print $4'}` disk in Failed Condition" >> /var/log/raid-health-check.log ;
+fi
+if [ `/opt/MegaRAID/MegaCli/MegaCli64 -AdpAllInfo -aAll | grep "Critical Disk" | awk {'print $4'}` -eq 0 ] && [ `/opt/MegaRAID/MegaCli/MegaCli64 -AdpAllInfo -aAll | grep "Failed Disk" | awk {'print $4'}` -eq 0 ] ;
+then
+    #SEND MAIL
+    echo "===> [`date`] <===" >> /var/log/raid-health-check.log ;
+    echo "--------------- DISK STATUS ---------------" >> /var/log/raid-health-check.log ;
+    echo `/opt/MegaRAID/MegaCli/MegaCli64 -PDList -aALL | awk -f analysis.awk` | sed "s/\,/\n/g"  >> /var/log/raid-health-check.log ;
+    echo "\n" >> /var/log/raid-health-check.log ;
+    echo "--------------- SYSTEM INFO ---------------" >> /var/log/raid-health-check.log ;
+    echo "`cat /etc/hosts`" >> /var/log/raid-health-check.log ;
+fi
+
+swaks --to sezer@sezer.test -from alert@sezer.test -s mail.sezer.test:587 -au alert@sezer.test -ap mailpassword --body /var/log/raid-health-check.log --header "Subject: Raid Disk Alarm"
+rm -rf /var/log/raid-health-check.log
+rm -rf CmdTool.log
+rm -rf MegaSAS.log
